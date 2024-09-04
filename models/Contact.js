54a750165e3d1a18f98f8ee1a1f4c9f7849fd5dd
MDABDULLAH1704 <mdabdullahqais123@gmail.com>
2024-09-04 12:00:20 +0530

    changes

--- models/Contact.js
+++ models/Contact.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose');
 
 const ContactSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    message: String
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+    },
+    subject: {
+        type: String,
+        required: [true, 'Subject is required']
+    },
+    message: {
+        type: String,
+        required: [true, 'Message is required']
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const ContactModel = mongoose.model('portfolioContact', ContactSchema);
